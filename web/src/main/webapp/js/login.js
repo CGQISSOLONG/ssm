12472a6bcdf0b674c2ca74a6298e1e9ba7e29bf8
caiguqoing <caiguoqing@newland.com.cn>
2020-10-29 18:28:41 +0800

    commit

---
+++ web/src/main/webapp/js/login.js
@@ -0,0 +1,9 @@
+function login(){
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+
+    console.log(username);
+    console.log(password);
+
+
+}
